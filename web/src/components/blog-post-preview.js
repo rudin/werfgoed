26e464b882ae38c2bba4fe492a2c83e2c151b1b5
Rudin Swagerman <post@rudinswagerman.nl>
2021-08-25 17:30:20 +0200

    video pointer events

--- web/src/components/blog-post-preview.js
+++ web/src/components/blog-post-preview.js
@@ -19,7 +19,7 @@ function BlogPostPreview(props) {
       >
         <div style={{ position: "relative", display: "flex", alignItems: "center" }}>
           <div className={noSlug ? styles.leadMediaThumbIntro : styles.leadMediaThumb}>
-            {noSlug && <div style={{ position: "relative", paddingBottom: `${100 / 16 * 9}%`, height: 0 }}><iframe src="https://player.vimeo.com/video/498264717?title=0&amp;byline=0&amp;portrait=0&amp;playsinline=0&amp;autopause=0&amp;app_id=122963&amp;h=8391841398" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="" title="Van de Werfklas naar het Werfgoed" style={{ width: "100%", height: "100%", position: "absolute", top: 0, left: 0 }}></iframe></div>}
+            {noSlug && <div style={{ position: "relative", paddingBottom: `${100 / 16 * 9}%`, height: 0, pointerEvents: "auto" }}><iframe src="https://player.vimeo.com/video/498264717?title=0&amp;byline=0&amp;portrait=0&amp;playsinline=0&amp;autopause=0&amp;app_id=122963&amp;h=8391841398" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="" title="Van de Werfklas naar het Werfgoed" style={{ width: "100%", height: "100%", position: "absolute", top: 0, left: 0 }}></iframe></div>}
             {!noSlug && props.mainImage && props.mainImage.asset && (
               <img
                 src={imageUrlFor(buildImageObj(props.mainImage))
